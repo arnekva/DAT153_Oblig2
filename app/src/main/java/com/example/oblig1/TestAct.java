a14b4469bf6d5ac03c2a733dd0429a501380f300
Arne Kvaleberg <arnekva@gmail.com>
2020-01-27 14:27:15 +0100

    Added placeholder to input fields
    Android:hints added to avoid having to manually delete placeholder text from input field

--- app/src/main/java/com/example/oblig1/TestAct.java
+++ app/src/main/java/com/example/oblig1/TestAct.java
@@ -8,7 +8,9 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -36,14 +38,11 @@ public class TestAct extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         imgView = findViewById(R.id.quizImageView);
-        //imgView.setImageResource(((GlobalStorage) this.getApplication()).getImages().get(2).getId());
-
         sb_ans = findViewById(R.id.submitText);
         sb_score = findViewById(R.id.tv_scoreText);
-       sb_submit = findViewById(R.id.submitAnswer);
+        sb_submit = findViewById(R.id.submitAnswer);
 
         initView();
-
     }
 
     private void initView(){
@@ -51,32 +50,45 @@ public class TestAct extends AppCompatActivity {
         sb_submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.println("ANSWER GIVEN WAS: " + sb_ans.getText().toString());
-                if (isCorrect(sb_ans.getText().toString(), correctAnswer)){
-                    score++;
-                    total++;
-                    Toast.makeText(getApplicationContext(),"Correct!",Toast.LENGTH_SHORT).show();
-                    sb_score.setText(score + "/" + total);
-                    sb_ans.getText().clear();
-                    setupList();
-
-                }else {
-                    total++;
-                    sb_score.setText(score + "/" + total);
-                    Toast.makeText(getApplicationContext(), "Incorrect! \nThe correct answer is: " + correctAnswer, Toast.LENGTH_LONG).show();
-                    sb_ans.getText().clear();
+                checkAnswer();
+            }
+        });
+        sb_ans.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_GO || (actionId == EditorInfo.IME_ACTION_DONE)) {
+                    //Perform your Actions here.
+                    checkAnswer();
                 }
+                return handled;
             }
         });
 
+    }
+    public void checkAnswer(){
+        if (isCorrect(sb_ans.getText().toString(), correctAnswer)){
+            score++;
+            total++;
+            Toast.makeText(getApplicationContext(),"Correct!",Toast.LENGTH_SHORT).show();
+            sb_score.setText(score + "/" + total);
+            sb_ans.getText().clear();
+            setupList();
+
+        }else {
+            total++;
+            sb_score.setText(score + "/" + total);
+            Toast.makeText(getApplicationContext(), "Incorrect! \nThe correct answer is: " + correctAnswer, Toast.LENGTH_LONG).show();
+            sb_ans.getText().clear();
+        }
     }
     public void setupList(){
         Random r = new Random();
         quiz = ((GlobalStorage) this.getApplication()).getImages();
         int next = r.nextInt(quiz.size());
-        quiz.remove(next); //Probably?
         imgView.setImageURI(quiz.get(next).getId());
         correctAnswer = quiz.get(next).getName();
+        quiz.remove(next); //Probably?
     }
     public void updateImage(){
 
